d6a5de991dbf0f12abdf091199fd540f9840d613
JaAguayo <jmaguayo@miners.utep.edu>
2020-11-08 21:52:33 -0700

    added comments

--- threadQueue.py
+++ threadQueue.py
@@ -1,20 +1,20 @@
 import threading
 
-class ProdConsQueue():
-    def __init__(self):
+class ProdConsQueue(): 
+    def __init__(self): #create an object with python lists, the semaphores will block the putFrame and getFrame if empty/full 
         self.queue = []
         self.lock = threading.Lock()
         self.full = threading.Semaphore(0)
         self.empty = threading.Semaphore(24)
 
-    def putFrame(self,frame):
+    def putFrame(self,frame): #enqueue in the lists
         self.empty.acquire()
         self.lock.acquire()
         self.queue.append(frame)
         self.lock.release()
         self.full.release()
 
-    def getFrame(self):
+    def getFrame(self):  #dequeue in the lists
         self.full.acquire()
         self.lock.acquire()
         frame = self.queue.pop(0)
--- threadedVideo.py
+++ threadedVideo.py
@@ -4,63 +4,65 @@ import cv2
 import threading
 from threadQueue import ProdConsQueue
 
+#based on the demo for extract frames
 def extract_frames(userFile, queue):
-    count = 0
+    count = 0  #initialize frame count 
     video_file = cv2.VideoCapture(userFile)
 
-    success,image = video_file.read()
+    success,image = video_file.read() #returns whether the frame is read correctly, and a frame
 
     print(f'Reading frame {count} {success}')
-    while success:
-        queue.putFrame(image)
-        success,image = video_file.read()
+    while success: 
+        queue.putFrame(image) #adding a frame to the queue
+        success,image = video_file.read() #get another frame
         print(f'Reading frame {count} {success}')
-        count += 1
+        count += 1 #increment the fram count
 
     print('Finished extracting frames')
-    queue.putFrame('$')
+    queue.putFrame('$') #putting a $ to show an empty queue
 
 def convert_gray(colorFrames, grayFrames):
-    count = 0
+    count = 0 #frame count
+    colorFrame = colorFrames.getFrame() #get a frame from queue of extracted color frames
 
-    colorFrame = colorFrames.getFrame()
-
-    while colorFrame is not '$':
+    while colorFrame is not '$': #as long as the extracted frames queue isnt empty 
         print(f'Converting frame {count}')
-        grayFrame = cv2.cvtColor(colorFrame,cv2.COLOR_BGR2GRAY)
+        grayFrame = cv2.cvtColor(colorFrame,cv2.COLOR_BGR2GRAY) #convert frame to gray and save it
 
-        grayFrames.putFrame(grayFrame)
-        count += 1
+        grayFrames.putFrame(grayFrame) #put the gray frame into a queue of gray frames
+        count += 1 #increment frame count
 
-        colorFrame = colorFrames.getFrame()
+        colorFrame = colorFrames.getFrame() #dequeue next color frame
 
     print('Conversion to grayscale complete')
-    grayFrames.putFrame('$')
+    grayFrames.putFrame('$') #if the gray queue is empty
 
 def display_frames(frames):
-    count = 0
-    frame = frames.getFrame()
+    count = 0 #frame count
+    frame = frames.getFrame() #get a frame from the gray queue
 
-    while frame is not '$':
+    while frame is not '$': #if the gray queue is not empty
         print(f'Displaying frame {count}')
-        cv2.imshow('Video',frame)
-        if cv2.waitKey(42) and 0xFF == ord("q"):
+        cv2.imshow('Video',frame) #display the frame in a window
+        if cv2.waitKey(42) and 0xFF == ord("q"): #wait 42ms and check if the user wants to quit
             break
-        count += 1
+        count += 1 #increment frame count
 
-        frame = frames.getFrame()
+        frame = frames.getFrame() #dequeue next gray frame
 
     print('Finished displaying all the frames')
-    cv2.destroyAllWindows()
+    cv2.destroyAllWindows() #cleanup all the windows that were displaying
 
 if __name__ == "__main__":
-    color_frames = ProdConsQueue()
-    gray_frames = ProdConsQueue()
+    color_frames = ProdConsQueue() #initialize the queue for color frames
+    gray_frames = ProdConsQueue()  #initialize the queue for the gray frames
 
+    #extract frames, convert frames, and display frames fuctions 
     extract = threading.Thread(target = extract_frames, args = ('clip.mp4',color_frames))
     convert = threading.Thread(target = convert_gray, args = (color_frames, gray_frames))
     display = threading.Thread(target = display_frames, args = (gray_frames,))
 
+    #starts the threads
     extract.start()
     convert.start()
     display.start()

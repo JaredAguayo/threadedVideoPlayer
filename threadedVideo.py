67541099aabe308bba634af18d14b54dacbe7b25
JaAguayo <jmaguayo@miners.utep.edu>
2020-11-08 20:10:35 -0700

    added the methods for extracting, converting, and displaying frames

--- threadedVideo.py
+++ threadedVideo.py
@@ -3,7 +3,7 @@
 import threading
 import cv2
 
-class ProConsQueue():
+class ProdConsQueue():
     def __init__(self,queueCapacity):
         self.queue = []
         self.full = Semaphore(0)
@@ -22,4 +22,57 @@ class ProConsQueue():
         frame = self.queue.pop(0)
         self.lock.release()
         self.empty.release()
-        return frame)
+        return frame
+
+    def extract_frames(userFile, queue):
+        count = 0
+        video_file = cv2.VideoCapture(userFile)
+
+        success,image = video_file.read()
+
+        while success:
+            queue.put(image)
+            success,image = video_file.read()
+            count += 1
+
+        queue.put('$')
+
+    def convert_gray(color. gray):
+        count = 0
+
+        colorFrame = color.get()
+
+        while colorFrame is not '$':
+            grayFrame = cv2.cvtColor(colorFrame,cv2.COLOR_BGR2GRAY)
+
+            gray.put(grayFrame)
+            count += 1
+
+            colorFrame = color.get()
+
+        grat.put('$')
+
+    def display_frames(frames):
+        count = 0
+        currentFrame = frames.get()
+
+        while currentFrame is not '$':
+            cv2.imshow('Video',currentFrame)
+            if cv2.waitKey(42) and 0xFF == ord("q"):
+                break
+            count += 1
+
+            currentFrame = frames.get()
+
+        cv2.destroyAllWindows()
+
+    color_frames = ProdConsQueue()
+    gray_frames = ProdConsQueue()
+
+    extract = threading.Thread(target = extract_frames, args = ('clip.mp4',color_frames))
+    convert = threading.Thread(target = convert_frames, args = (color_frames, gray_frames))
+    display = threading.Thread(target = display_frames, args = (gray_frames))
+
+    extract.start()
+    convert.start()
+    display.start()
